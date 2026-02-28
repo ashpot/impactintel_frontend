b807b35e845b73bda54b6656676cc3c6ec811a2e
Mark Orrente <markorrente01@gmail.com>
2026-02-28 04:55:06 +0100

    hero section

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import App from '@/app/App'
 import { BrowserRouter } from 'react-router-dom'
 import "@fontsource/lato";
 import "@fontsource/lato/700.css";
+import '@fontsource-variable/plus-jakarta-sans';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/shared/components/Navbar.tsx
+++ src/shared/components/Navbar.tsx
@@ -1,12 +1,14 @@
 import brandLogo from '@/assets/brand/brand_logo.png';
 import { cn } from '@/shared/utils/cn';
+import RequestBtn from '../ui/RequestBtn';
 const Navbar = () => {
-  const buttonClass = 'border rounded-full py-3 px-4 font-medium text-base';
-  const linkClass = 'hover:cursor-pointer font-medium text-text-muted py-3 px-4 transition-all duration-100'
+  const buttonClass = 'border rounded-full py-3 px-4 font-medium text-base transition-all duration-300';
+  const linkClass = cn('hover:cursor-pointer hover:text-brand-primary font-medium', 
+    'text-text-muted py-3 px-4 transition-all duration-300')
   return (
-    <nav className='w-full fixed font-lato g-pad w-full text-white border-b border-border-primary'>
+    <nav className='w-full fixed font-lato px-30 w-full text-white border-b border-border-primary'>
       <section className='py-7 flex items-center justify-between'>
-        <div className='w-[230px]'>
+        <div className='w-[250px]'>
           <img src={brandLogo} alt="brand logo" />
         </div>
         {/* links */}
@@ -16,17 +18,14 @@ const Navbar = () => {
             <li className={linkClass}>Contact</li>
           </ul>
         {/* quick actions */}
-        <div className='space-x-2'>
+        <div className='space-x-2 flex'>
           <button className={cn('border-border-primary text-text-muted',
+            'hover:bg-brand-primary hover:border-bg-brand-primary  hover:text-text-title',
             buttonClass
           )}>
             Client Login
           </button>
-          <button className={cn('border-brand-primary text-text-title bg-brand-primary',
-            buttonClass
-          )}>
-            Request a Demo
-          </button>
+          <RequestBtn />
         </div>
       </section>
         

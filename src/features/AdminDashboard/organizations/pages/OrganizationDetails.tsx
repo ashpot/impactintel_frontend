1d6ab26fce10ad113735353f11cc998d4379e059
Mark Orrente <markorrente01@gmail.com>
2026-03-30 20:53:39 +0100

    admin: organizations subpage done

--- src/features/AdminDashboard/organizations/pages/OrganizationDetails.tsx
+++ src/features/AdminDashboard/organizations/pages/OrganizationDetails.tsx
@@ -57,8 +57,8 @@ const OrganizationDetails = () => {
             {orgInfo01.map(({label, value}, i)=>{
                 return(
                     <div className="w-full space-y-3" key={i}>
-                        <p className="text-text-body text-xs">{label}</p>
-                        <h3 className={`text-sm font-bold tracking-wide ${value === 'Active' ? 'text-success': 'text-text-primary01'}`}>{value}</h3>
+                        <p className="text-text-body text-sm">{label}</p>
+                        <h3 className={`text-base font-bold tracking-wide ${value === 'Active' ? 'text-success': 'text-text-primary01'}`}>{value}</h3>
                     </div>
                 )
             })}
@@ -69,18 +69,86 @@ const OrganizationDetails = () => {
        <section>
         <Container
             title="Organization Details"
-            className="grid grid-cols-2 gap-x-5"
+            className="grid grid-cols-2 gap-x-5 pb-0"
         >
             {orgInfo02.map(({label, value}, i)=>{
                 return(
-                    <div className="w-full space-y-1.5 mb-6 last:mb-0" key={i} >
-                        <p className="text-text-body text-xs">{label}</p>
-                        <h3 className="text-sm font-bold tracking-wide">{value}</h3>
+                    <div className="w-full space-y-1.5 mb-6" key={i} >
+                        <p className="text-text-body text-sm">{label}</p>
+                        <h3 className="text-base font-bold tracking-wide">{value}</h3>
                     </div>
                 )
             })}
         </Container>
+       </section>
 
+       <section className="flex gap-8">
+        <Container
+            title="Users Overview"
+            className="w-full"
+        >
+            <p className="text-text-body text-sm mb-5">Total Users: 45</p>
+            <div className="space-y-4">
+                {[
+                {label: 'Client Admin', value: '3'},
+                {label: 'CSR Officer', value: '28'},
+                {label: 'Auditor', value: '14'}
+            ].map(({label, value}, i)=>{
+                return(
+                    <div className="flex items-center justify-between" key={i}>
+                        <p className="text-base font-bold tracking-wide text-text-primary01">{label}</p>
+                        <p className="text-base font-bold tracking-wide text-shadow-text-primary01">{value}</p>
+                    </div>
+                    
+                )
+            })
+            }
+            </div>
+            <Button
+                variant="ghost"
+                className="text-base font-bold mt-6 px-0 hover:bg-transparent hover:cursor-pointer"
+            >
+                View All Users
+            </Button>
+        </Container>
+        <Container
+            title="Reviews & Flags"
+            className="w-full"
+        >
+            <p className="text-text-body text-sm">No pending reviews of flags.</p>
+        </Container>
+       </section>
+
+       <section>
+        <Container
+            title="System Activity"
+        >
+            <div className="border-b border-line flex items-center py-5 justify-between">
+                <div className="w-full space-y-2">
+                    <h3 className="text-text-primary01 text-base font-bold capitalize">Approval</h3>
+                    <p className="text-text-body text-sm">Organization approved by Super Admin</p>
+                </div>
+                <div className="text-text-body text-sm w-30">
+                    Dec 15, 2024
+                </div>
+            </div>
+            <div className="border-b border-line flex items-center py-5 justify-between">
+                <div className="w-full space-y-2">
+                    <h3 className="text-text-primary01 text-base font-bold capitalize">Approval</h3>
+                    <p className="text-text-body text-sm">Organization approved by Super Admin</p>
+                </div>
+                <div className="text-text-body text-sm w-30">
+                    Dec 15, 2024
+                </div>
+            </div>
+            
+            <Button
+                variant="ghost"
+                className="text-base font-bold mt-6 px-0 hover:bg-transparent hover:cursor-pointer"
+            >
+                View Full Audit Logs
+            </Button>
+        </Container>
        </section>
     </div>
   )
